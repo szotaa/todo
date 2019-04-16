0c43b4814460a352364a7f7316f680f7ee35b90d
szotaa <kubaszota97@gmail.com>
2019-04-16 19:07:06 +0200

    Initial store in auth module

---
+++ src/app/auth/+state/auth.reducers.ts
@@ -0,0 +1,20 @@
+import { AuthActions, AuthActionTypes } from './auth.actions';
+
+export interface State {
+
+}
+
+export const initialState: State = {
+
+};
+
+export function reducer(state = initialState, action: AuthActions): State {
+  switch (action.type) {
+
+    case AuthActionTypes.LoadAuths:
+      return state;
+
+    default:
+      return state;
+  }
+}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginUiComponent } from './components/login-ui/login-ui.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuthRoutingModule} from './auth-routing.module';
+import {LoginUiComponent} from './components/login-ui/login-ui.component';
 import {FormsModule} from '@angular/forms';
-import { LoginComponent } from './containers/login/login.component';
+import {LoginComponent} from './containers/login/login.component';
+import {StoreModule} from '@ngrx/store';
+import * as fromAuth from './+state/auth.reducers';
+import {EffectsModule} from '@ngrx/effects';
+import {AuthEffects} from './+state/auth.effects';
 
 @NgModule({
   declarations: [
@@ -13,7 +17,9 @@ import { LoginComponent } from './containers/login/login.component';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    FormsModule
+    FormsModule,
+    StoreModule.forFeature('auth', fromAuth.reducer),
+    EffectsModule.forFeature([AuthEffects])
   ]
 })
 export class AuthModule { }
