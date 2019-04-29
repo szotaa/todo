958f1ac3eaf0665dd5980cff2f6f4e6cd144bd19
szotaa <kubaszota97@gmail.com>
2019-04-29 10:30:24 +0200

    Refactor repeating modules declarations into common shared module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from './auth-routing.module';
 import {LoginUiComponent} from './components/login-ui/login-ui.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from './containers/login/login.component';
 import {StoreModule} from '@ngrx/store';
 import * as fromAuth from './+state/auth.reducers';
 import {EffectsModule} from '@ngrx/effects';
 import {AuthEffects} from './+state/auth.effects';
-import { RegisterUiComponent } from './components/register-ui/register-ui.component';
-import { RegisterComponent } from './containers/register/register.component';
+import {RegisterUiComponent} from './components/register-ui/register-ui.component';
+import {RegisterComponent} from './containers/register/register.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,11 +20,10 @@ import { RegisterComponent } from './containers/register/register.component';
   ],
   imports: [
     CommonModule,
+    SharedModule,
     AuthRoutingModule,
-    FormsModule,
     StoreModule.forFeature('auth', fromAuth.reducer),
-    EffectsModule.forFeature([AuthEffects]),
-    ReactiveFormsModule
+    EffectsModule.forFeature([AuthEffects])
   ]
 })
 export class AuthModule { }
